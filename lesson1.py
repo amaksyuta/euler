38eca4a8232efaccc9d1bab3b39ca335e43cd04d
amaksyuta <a@itech.ua>
2018-09-09 17:23:23 +0300

    Create first construction

--- lesson1.py
+++ lesson1.py
@@ -1,7 +1,12 @@
-print("\n [INFO]: First Euler lesson")
+print("\n[INFO]: First Euler lesson")
 i = 0
-while i < 10:
-    i += 1
-    if i % 2 & i % 3 == 0:
-        print(i)
+SUMA = 0
+
+while i < 9:
+	i += 1
+	if i % 3 == 0 or i % 5 == 0:
+		print("\n", i)
+		SUMA +=i
+
+print("\n[INFO]: Summa is", SUMA)
 
