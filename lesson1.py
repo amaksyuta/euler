e437af1954e91e6b3490b0e0f9bb43e9e95525a1
amaksyuta <a@itech.ua>
2018-09-09 16:59:20 +0300

    Use last changes

--- lesson1.py
+++ lesson1.py
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-print("[INFO]: First Euler lesson")
-i = 0
-while i < 10:
-    i += 1
-    print(i)
-=======
 print("\n [INFO]: First Euler lesson")
 i = 0
 while i < 10:
     i += 1
     if i % 2 & i % 3 == 0:
         print(i)
->>>>>>> 17083bb698b4b4efe86a1f92b6c63146211d3d91
+
