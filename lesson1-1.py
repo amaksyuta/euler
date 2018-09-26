460828986bef26a02b5a21686700a3909c818777
amaksyuta <a@itech.ua>
2018-09-26 22:20:32 +0300

    Cosmetic fix

--- lesson1-1.py
+++ lesson1-1.py
@@ -1,4 +1,4 @@
-print("\n[INFO]:First EULER lesson with function")
+print("\n[INFO]: First EULER lesson with function")
 def calculMultipleNumbers(number):
     """Function for return Summary of some numbers range"""
     return sum(n for n in range(number) if n % 3 == 0 or n % 5 == 0)
