b7f9e0650448214958596a63175fb457f9e5d990
amaksyuta <a@itech.ua>
2018-11-12 13:01:49 +0200

    Add modify

--- lesson2.py
+++ lesson2.py
@@ -14,10 +14,27 @@ def get_fib_summ(l):
     """Function for return summa of elements Fibonacci"""
     return sum(n for n in l if n % 2 == 0)
 #############################################################
-n = 1000
-lst = get_fib_numbers(n)
-print(lst)
-a = get_fib_summ(lst)
-print(a)
+def fib(n):
+    if n in M:
+        return M[n]
+    M[n] = fib(n - 1) + fib(n - 2)
+    return M[n]
+#############################################################
+def fib1(n):
+    a = 0
+    b = 1
+    for __ in range(n):
+        a, b = b, a + b
+    return a
+#############################################################
+n = 10
+#a = get_fib_summ(get_fib_numbers(n))
+
+M = {0: 0, 1: 1}
+
+
+a = fib1(n)
+for i in range(a):
+    print(i)
 
 
